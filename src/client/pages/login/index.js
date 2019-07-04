185d37721790673c88ef7f9222bcc694d68d2a21
narayanvaishali <narayanvaishali@gmail.com>
2019-07-04 19:01:20 +0100

    Testing Backendless api requests

--- src/client/pages/login/index.js
+++ src/client/pages/login/index.js
@@ -3,6 +3,7 @@ import { Link,Redirect } from 'react-router-dom';
 //import { Redirect } from 'react-router';
 import axios from 'axios';
 import { config } from '../../utils/Config';
+import Backendless from 'backendless';
 
 export default class Login extends React.Component {
 
@@ -64,16 +65,44 @@ export default class Login extends React.Component {
         return res;
     }).catch(err => err);   */
 
-    fetch('data.json', {
+ ///Get request
+  /*Backendless.Data.of( "LDNUsers" ).find()
+  .then( function( result ) {
+     // every loaded object from the "Contact" table is now an individual untyped
+     // JS object in the "result" array
+     console.log('result is' + JSON.stringify(result));
+   })
+  .catch( function( error ) {
+    // an error has occurred, the error code can be retrieved with fault.statusCode
+   });*/
+
+   //update request
+  /* data = [{"address":"10880 Malibu Point","created":1562255385597,"name":"Tony Stark","id":"1","updated":null,"objectId":"14C502C6-41BB-82C2-FFCC-9EC05AA5E300","ownerId":null,"___class":"LDNUsers"},{"address":"New York City, Brooklyn","created":1562255385597,"name":"Steve Rogers","id":"3","updated":null,"objectId":"157F16DC-3E92-C198-FF98-4C9AECAE7C00","ownerId":null,"___class":"LDNUsers"},{"address":"Palace in Asgard","created":1562255385597,"name":"Thor Odinson","id":"4","updated":null,"objectId":"9F43A8AF-6BEE-1CA0-FF58-A361A8F11300","ownerId":null,"___class":"LDNUsers"},{"address":"2766 Taylor Street","created":1562255385597,"name":"Bruce Banner","id":"2","updated":null,"objectId":"BC601CAD-28FB-A0D4-FFC4-3F17351FD700","ownerId":null,"___class":"LDNUsers"}]*/
+
+/*
+  data = {"address":"10880 Malibu Point","name":"Tony Stark C","id":"1","objectId":"14C502C6-41BB-82C2-FFCC-9EC05AA5E300"}
+
+  Backendless.Data.of( "LDNUsers" ).save( data )
+  .then( function( savedObject ) {
+      console.log(JSON.stringify(savedObject));
+      console.log( "LDNUsers instance has been updated" );
+    })
+  .catch( function( error ) {
+      console.log( "an error has occurred " + error.message );
+    });
+*/
+
+   /* fetch('http://myjson.com/7x3fz', {
                 method: 'POST',
                 headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
               },
                 body:JSON.stringify({name: "Graham"})
-            }).then((res) => res.json())
+            }).then((res) => res)
             .then((data) =>  console.log(data))
-            .catch((err)=>console.log('here ' + err))      
+            .catch((err)=>console.log('here login error -> ' + err))      
+    */
 
 		/* axios.post(config.baseUrl + 'login', params)
 		    .then(response => {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Redirect } from 'react-router-dom';
 import AppRoutes from '../src/client/components/AppRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Backendless from 'backendless';
+ 
+Backendless.initApp('F7C10331-A8CC-D8D1-FFB5-6722FAE56000', '6C03435C-3642-81F6-FF3D-CBF0D362A800');
 
 const app = document.getElementById('app');
 
